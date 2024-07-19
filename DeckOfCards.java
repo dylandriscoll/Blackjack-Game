eb556ad88250f05f02e82a14405ab15ca8ea069c
Dylan Driscoll <123216156+dylandriscoll@users.noreply.github.com>
2024-07-19 08:58:04 -0700

    Update DeckOfCards.java

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,18 +1,10 @@
-
-/*This is my deck of cards class. It creates a deck
-as a linked list, adds all the cards using for loops,
-shuffles the deck, uses collections to shuffle the 
-cards, then adds the cards to a queue.*/
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class DeckOfCards {
-
-  /*This class will create a linked list deck object, fill
-  it with all 52 cards, shuffle it, put those cards into
-  a Queue<Card> object and return the Queue<Card>.*/
+  
   public static Queue<Card> InitializeDeck() {
     List<Card> deck = new LinkedList<>();
 
